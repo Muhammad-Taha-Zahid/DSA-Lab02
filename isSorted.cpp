d6dca9ca39db80c62ec1c69c72a7b760a9827849
Taha <muzahid.bese25seecs@seecs.edu.pk>
2026-09-18 21:52:54 +0500

    written initial test cases

---
+++ isSorted.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cassert> //for assert function
+using namespace std;
+
+bool isSorted(const int* arr, const int size); //prototype function
+//TEST CASES
+void testSortedArray() { //example tests from lab doc
+    int arr[] = {1, 2, 3, 4, 5};
+    assert(isSorted(arr, 5) == true);
+}
+
+void testUnsortedArray() {
+    int arr[] = {1, 4, 3, 5, 6};
+    assert(isSorted(arr, 5) == false);
+}
+
+void testDuplicateValues() {
+    int arr[] = {1, 2, 2, 3, 5};
+    assert(isSorted(arr, 5) == true);
+}
+
+void testSingleElement() {
+    int arr[] = {42};
+    assert(isSorted(arr, 1) == true);
+}
+
+void testDescendingArray() {
+    int arr[] = {5, 4, 3, 2, 1};
+    assert(isSorted(arr, 5) == false);
+}
+
+void testNegativeValues() {
+    int arr[] = {-10, -5, 0, 5, 10};
+    assert(isSorted(arr, 5) == true);
+}
+
+// run all test cases
+void runAllTests() {
+    testSortedArray();
+    testUnsortedArray();
+    testDuplicateValues();
+    testSingleElement();
+    testDescendingArray();
+    testNegativeValues();
+    // will only print if all assertions pass, otherwise program will terminate on a failed assertion
+    cout << "All TDD assertions passed successfully!" << endl;
+}
+
+//dummy function
+bool isSorted(const int* arr, const int size) {
+    return false;
+}
+
+int main() {
+    // run all test cases
+    runAllTests();
+    return 0;
+}
+
